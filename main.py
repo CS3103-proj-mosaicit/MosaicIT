197cbee268f47b23674772461fe828a0365660b6
April0616 <594395096@qq.com>
2019-11-07 15:57:55 +0800

    Finalize Flask Conversion of pages

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import os  # for image upload
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
 	return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+	return render_template('gallery.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+	# if request.method == 'POST':
+ #        # return redirect(url_for('index'))
+ #        return
+    return render_template('upload.html')
 
 
 if __name__ == '__main__':
