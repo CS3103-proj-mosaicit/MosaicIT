ced96cc88a81432635eff887b21766239aa6c949
lucydotc <lucy@u.nus.edu>
2019-11-10 04:25:12 +0800

    update BW

--- mysql/testing_collage.py
+++ mysql/testing_collage.py
@@ -1,5 +1,6 @@
 import ImageDB
 from PIL import Image
+from PIL import ImageEnhance
 import io
 from image_crawler import store_raw_images
 import base64
@@ -9,7 +10,7 @@ SCALE= 20
 
 
 #src_img_path = './tea.jpg'
-src_img_path = input("Please enter the path to the src img")
+src_img_path = input("Please enter the path to the src img\n")
 
 
 db = ImageDB.db()
@@ -19,12 +20,12 @@ db.init()
 #store_raw_images("sky")
 print("Skipping crawl testing...")
 
-im = Image.open(src_img_path)
+im = Image.open(src_img_path).convert('LA')
 #imm = im.thumbnail((15, 15))
 px = im.load() #px is 2d matrix of pixel coordinates
 #im.show()
 width, height, = im.size
-print('Resizing width to ' + str(width) + ' and height to ' + str(height))
+print('Size of width ' + str(width) + ' and height ' + str(height))
 
 canvas_width = width*SCALE
 canvas_height = height*SCALE
@@ -39,39 +40,49 @@ canvas = Image.new('RGB', (canvas_width, canvas_height), (0, 0, 255))
 num_tiles_needed = width * height
 
 print("Opening "+ str(num_tiles_needed) + " testing images...")
-#tiles_selected = db.select_num(num_tiles_needed)
-tiles_selected = db.select_num(30)
+tiles_selected = db.select_num(num_tiles_needed)
 
-for i in range(0, width):
-    canvas_x = i*SCALE
-    for j in range(0, height):
-        canvas_y = j*SCALE 
+try:
+    for i in range(0, width):
+        canvas_x = i*SCALE
+        for j in range(0, height):
+            canvas_y = j*SCALE 
         
-        print('px[' +str(i) +',' +str(j)+ ']'+ ' is ' + str(px[i,j]))
-        px_r = px[i,j][0]
-        px_g = px[i,j][1]
-        px_b = px[i,j][2]
-        hexcolour = db.rgb_to_hex(px_r,px_g,px_b)
-        print(type(hexcolour))
+            print('px[' +str(i) +',' +str(j)+ ']'+ ' is ' + str(px[i,j]))
+            px_l = px[i,j][0]
+            px_a = px[i,j][1]
          
-        print("Selecting pic close to " + str(px[i,j]))
-        img = db.select_rough_rgb(hexcolour)
-        #img = db.select_closest_rgb((px[i,j]))   
-        #print(base64.b64encode(img))
-       
-        rgb=db.avg_rgb(img)
-        r, g, b = rgb // 65536, (rgb - rgb // 65536 * 65536) // 256, rgb % 256
-        print("Input rgb: " +str((px_r, px_g, px_b)) +", output rgb:" + str((r, g, b)))
-        #db.raw_to_img(img).show() 
-        img = db.raw_to_img(img)
-        #print(str(type(img))) #giving back nontype
-        img.thumbnail((SCALE,SCALE))
-        #print(str(type(img))) #giving back nontype
-
+            print("Selecting pic close to " + str(px[i,j]))
         
-        #pasting here
-        canvas.paste(img, (canvas_x, canvas_y))
-        canvas.save("./mosaic.jpg")
+            identifier = j%74
+            if identifier == 0:
+                identifier = 75
 
+            print(str(identifier))
+            db.cursor.execute("SELECT img FROM imagedb.image WHERE id=%s;", (identifier,))
 
-canvas.show()      
+            img = db.cursor.fetchone()
+            img = db.raw_to_img(img[0])
+        
+            img.thumbnail((SCALE,SCALE))
+            greyimg = img.convert('LA')
+            #print(str(type(img))) #giving back nontype
+        
+    
+            #brightness lvl here
+        
+            #factor = (260 - px_l)/200
+            factor = px_l/255
+            print(str(factor))
+            enhancer_object = ImageEnhance.Brightness(greyimg)
+            outimg = enhancer_object.enhance(factor) 
+
+            #pasting here
+            canvas.paste(outimg, (canvas_x, canvas_y))
+
+except KeyboardInterrupt:
+    print('\nStopping, saving partial image...')
+
+finally:
+    canvas.save("./mosaic.jpg")
+    canvas.show()      
